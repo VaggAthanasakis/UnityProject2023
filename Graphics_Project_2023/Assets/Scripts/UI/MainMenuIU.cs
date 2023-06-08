9a31f6345113871f6810b7107dc371c228937a29
VaggAthanasakis <91693054+VaggAthanasakis@users.noreply.github.com>
2023-06-08 15:20:13 +0300

    character selection + main menu

--- Graphics_Project_2023/Assets/Scripts/UI/MainMenuIU.cs
+++ Graphics_Project_2023/Assets/Scripts/UI/MainMenuIU.cs
@@ -8,6 +8,7 @@ public class MainMenuIU : MonoBehaviour {
     //private const string MAIN_GAME_SCENE = "MainGameScene";
 
     [SerializeField] GameObject buttonsMainMenu;
+    [SerializeField] GameObject creditsPanelInfo;
 
     private void Start() {
        // SoundManager.Instance.PlaySoundWithoutFade(SoundManager.MAIN_MENU_CHAR_SELECTION_MUSIC);
@@ -23,7 +24,14 @@ public class MainMenuIU : MonoBehaviour {
     public void Button_Credits() {
         SoundManager.Instance.PlaySoundWithoutFade(SoundManager.BUTTON_PRESS);
         buttonsMainMenu.SetActive(!buttonsMainMenu.activeSelf);
+        creditsPanelInfo.SetActive(true);
 
+    }
+
+    public void Button_CloseCredits() {
+        SoundManager.Instance.PlaySoundWithoutFade(SoundManager.BUTTON_PRESS);
+        creditsPanelInfo.SetActive(false);
+        buttonsMainMenu.SetActive(true);
     }
 
     public void Button_Quit() {
